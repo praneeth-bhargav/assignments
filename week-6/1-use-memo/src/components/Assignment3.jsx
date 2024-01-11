b630adcb16627b992eff438cca87834b0c6c6aff
praneeth-bhargav <praneethbhargav44@gmail.com>
2024-01-11 07:40:05 +0530

    assignment completed

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,9 @@ const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+       return items.reduce((acc,item)=>acc+item.value,0);
+    },[items]);
     // Your code ends here
     return (
         <div>
